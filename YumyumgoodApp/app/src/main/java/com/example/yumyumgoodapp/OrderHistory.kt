0119ea39bfb2d3726e3832e06e80eccdfc0af45c
Jisoo Park <cosmopark624@gmail.com>
2021-05-27 02:03:32 +0900

    [feat]:connect activity

--- YumyumgoodApp/app/src/main/java/com/example/yumyumgoodapp/OrderHistory.kt
+++ YumyumgoodApp/app/src/main/java/com/example/yumyumgoodapp/OrderHistory.kt
@@ -1,11 +1,23 @@
 package com.example.yumyumgoodapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.order_history.*
 
 class OrderHistory : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.order_history)
+
+        btnWriteReview.setOnClickListener{
+
+            val intent = Intent(this, WriteReview::class.java)
+            startActivity(intent)
+
+        }
     }
 }
--- YumyumgoodApp/app/src/main/java/com/example/yumyumgoodapp/WriteReview.java
+++ YumyumgoodApp/app/src/main/java/com/example/yumyumgoodapp/WriteReview.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +19,13 @@ public class WriteReview extends AppCompatActivity {
     EditText write_review;
     TextView count_letter;
     Button submit_button;
+    ImageButton btnLike1;
+    ImageButton btnDislike1;
+    ImageButton btnLike2;
+    ImageButton btnDislike2;
+    ImageButton btnLike3;
+    ImageButton btnDislike3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,61 @@ public class WriteReview extends AppCompatActivity {
         write_review = (EditText) findViewById(R.id.writer);
         count_letter = (TextView) findViewById(R.id.counter);
         submit_button= (Button) findViewById(R.id.submit_review);
+
+        btnLike1 = (ImageButton) findViewById(R.id.like1);
+        btnDislike1 = (ImageButton) findViewById(R.id.dislike1);
+        btnLike2 = (ImageButton) findViewById(R.id.like2);
+        btnDislike2 = (ImageButton) findViewById(R.id.dislike2);
+        btnLike3 = (ImageButton) findViewById(R.id.like3);
+        btnDislike3 = (ImageButton) findViewById(R.id.dislike3);
+
+        btnLike1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnLike1.setSelected(true);
+                btnDislike1.setSelected(false);
+            }
+        });
+
+        btnDislike1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnLike1.setSelected(false);
+                btnDislike1.setSelected(true);
+            }
+        });
+        btnLike2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnLike2.setSelected(true);
+                btnDislike2.setSelected(false);
+            }
+        });
+
+        btnDislike2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnLike2.setSelected(false);
+                btnDislike2.setSelected(true);
+            }
+        });
+        btnLike3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnLike3.setSelected(true);
+                btnDislike3.setSelected(false);
+            }
+        });
+
+        btnDislike3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnLike3.setSelected(false);
+                btnDislike3.setSelected(true);
+            }
+        });
+
+
 
 
         submit_button.setOnClickListener(new View.OnClickListener(){
